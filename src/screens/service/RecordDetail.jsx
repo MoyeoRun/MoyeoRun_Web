29adeb49bd5447e282b9e4e1aacbb6c923faf2c2
Inseo Hwang <sjsjsj1246@gmail.com>
2021-11-10 19:34:29 +0900

    Feat: 테스트 데이터 추가

--- src/screens/service/RecordDetail.jsx
+++ src/screens/service/RecordDetail.jsx
@@ -6,103 +6,121 @@ import Text from '../../components/Text';
 import CustomButton from '../../components/CustomButton';
 import { getDistanceString, getPaceString, recordTimeString } from '../../lib/util/strFormat';
 import { ReactComponent as EditIcon } from '../../assets/svgs/EditIcon.svg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import recordDetailData from '../../testData/recordDetailData.json';
+import { useLocation } from 'react-router';
 
-const recordData = {
+const data = {
   date: new Date().toString(),
   title: '바람부는 날 5Km 함께 뛰어요',
   distance: 5,
   pace: 5.36,
   time: 1700,
+  runData: [
+    { latitude: 37.659187827620975, longitude: 127.0514252126567 },
+    { latitude: 37.65703042721502, longitude: 127.05266975756653 },
+    { latitude: 37.653191118834805, longitude: 127.0534422337102 },
+    { latitude: 37.65312316468623, longitude: 127.0548155246323 },
+    { latitude: 37.65599417327399, longitude: 127.05449365957243 },
+    { latitude: 37.66012211532027, longitude: 127.052326434836 },
+    { latitude: 37.66441968764726, longitude: 127.05174707772825 },
+    { latitude: 37.66779131629595, longitude: 127.0497408063072 },
+    { latitude: 37.66776583850865, longitude: 127.04826022703183 },
+    { latitude: 37.666118256361884, longitude: 127.04911853385815 },
+    { latitude: 37.66453008843758, longitude: 127.05060984196885 },
+    { latitude: 37.66149803713027, longitude: 127.05093170702871 },
+    { latitude: 37.659553042638635, longitude: 127.05138231811692 },
+    { latitude: 37.6556629007224, longitude: 127.05346371235336 },
+  ],
+  runSummaryData: [
+    {
+      pace: 5.0,
+      altitude: -2,
+    },
+    {
+      pace: 5.3,
+      altitude: -3,
+    },
+    {
+      pace: 5.22,
+      altitude: -4,
+    },
+    {
+      pace: 5.1,
+      altitude: -1,
+    },
+  ],
 };
 
-const runData = [
-  {
-    pace: 5.0,
-    altitude: -2,
-  },
-  {
-    pace: 5.3,
-    altitude: -3,
-  },
-  {
-    pace: 5.22,
-    altitude: -4,
-  },
-  {
-    pace: 5.1,
-    altitude: -1,
-  },
-];
-
 const RecordDetail = () => {
+  const [data, setData] = useState(null);
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    console.log(window.kakao);
-    var container = document.getElementById('map');
-    var options = {
-      center: new window.kakao.maps.LatLng(37.66149803713027, 127.05093170702871),
-      level: 5,
-    };
-    var map = new window.kakao.maps.Map(container, options);
-    var polyline = new window.kakao.maps.Polyline({
-      map: map,
-      path: [
-        new window.kakao.maps.LatLng(37.659187827620975, 127.0514252126567),
-        new window.kakao.maps.LatLng(37.65703042721502, 127.05266975756653),
-        new window.kakao.maps.LatLng(37.653191118834805, 127.0534422337102),
-        new window.kakao.maps.LatLng(37.65312316468623, 127.0548155246323),
-        new window.kakao.maps.LatLng(37.65599417327399, 127.05449365957243),
-        new window.kakao.maps.LatLng(37.66012211532027, 127.052326434836),
-        new window.kakao.maps.LatLng(37.66441968764726, 127.05174707772825),
-        new window.kakao.maps.LatLng(37.66779131629595, 127.0497408063072),
-        new window.kakao.maps.LatLng(37.66776583850865, 127.04826022703183),
-        new window.kakao.maps.LatLng(37.666118256361884, 127.04911853385815),
-        new window.kakao.maps.LatLng(37.66453008843758, 127.05060984196885),
-        new window.kakao.maps.LatLng(37.66149803713027, 127.05093170702871),
-        new window.kakao.maps.LatLng(37.659553042638635, 127.05138231811692),
-        new window.kakao.maps.LatLng(37.6556629007224, 127.05346371235336),
-      ],
-      strokeWeight: 7,
-      strokeColor: '#1162FF',
-      strokeOpacity: 0.8,
-    });
-    new window.kakao.maps.Marker({
-      map: map,
-      position: new window.kakao.maps.LatLng(37.659187827620975, 127.0514252126567),
-    }).setMap(map);
-    new window.kakao.maps.Marker({
-      map: map,
-      position: new window.kakao.maps.LatLng(37.6556629007224, 127.05346371235336),
-    }).setMap(map);
-    polyline.setMap(map);
+    setData(recordDetailData);
   }, []);
+
+  useEffect(() => {
+    if (data) {
+      var container = document.getElementById('map');
+      var options = {
+        center: new window.kakao.maps.LatLng(37.659187827620975, 127.0514252126567),
+        level: 5,
+      };
+      var map = new window.kakao.maps.Map(container, options);
+      new window.kakao.maps.Polyline({
+        map: map,
+        path: [
+          data.runData.map((point) => {
+            return new window.kakao.maps.LatLng(point.latitude, point.longitude);
+          }),
+        ],
+        strokeWeight: 7,
+        strokeColor: '#1162FF',
+        strokeOpacity: 0.8,
+      }).setMap(map);
+      new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(data.runData[0].latitude, data.runData[0].longitude),
+      }).setMap(map);
+      new window.kakao.maps.Marker({
+        map: map,
+        position: new window.kakao.maps.LatLng(
+          data.runData[data.runData.length - 1].latitude,
+          data.runData[data.runData.length - 1].longitude,
+        ),
+      }).setMap(map);
+    }
+  }, [data]);
+
+  if (!data) return null;
 
   return (
     <Box css={RecordDetailWrapper}>
-      <Text css={recordDate}>{new Date(recordData.date).toLocaleString()}</Text>
+      <Text css={recordDate}>{new Date(data.date).toLocaleString()}</Text>
       <Box css={recordTitleWrapper}>
-        <Text>{recordData.title}</Text>
+        <Text>{data.title}</Text>
         <EditIcon />
       </Box>
       <div id="map" style={{ width: '100%', height: '430px' }}></div>
       <Box css={recordStatusWrapper}>
         <Box css={recordStatusItem}>
-          <Text className="value">{getDistanceString(recordData.distance)}</Text>
+          <Text className="value">{getDistanceString(data.distance)}</Text>
           <Text className="key">거리</Text>
         </Box>
         <Box css={recordStatusItem}>
-          <Text className="value">{getPaceString(recordData.pace)}</Text>
+          <Text className="value">{getPaceString(data.pace)}</Text>
           <Text className="key">평균 페이스</Text>
         </Box>
         <Box css={recordStatusItem}>
-          <Text className="value">{recordTimeString(recordData.time)}</Text>
+          <Text className="value">{recordTimeString(data.time)}</Text>
           <Text className="key">시간</Text>
         </Box>
       </Box>
       <CustomButton css={recordAnalysisButton}>상세 분석 보기</CustomButton>
       <Box>
         <Text css={{ fontFamily: 'text-500', fontSize: '22px' }}>구간</Text>
-        <RecordDetailTable data={runData} />
+        <RecordDetailTable data={data.runSummaryData} />
       </Box>
     </Box>
   );
