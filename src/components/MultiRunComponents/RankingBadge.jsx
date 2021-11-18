bf70b644c731cf0ecb057b6b7d658b8438c0d43d
Sangjun-man <molung@icloud.com>
2021-11-18 16:44:38 +0900

    Feat : 멀티런 뷰 순위 뱃지 디테일 수정

--- src/components/MultiRunComponents/RankingBadge.jsx
+++ src/components/MultiRunComponents/RankingBadge.jsx
@@ -2,30 +2,72 @@
 import { css } from '@emotion/react';
 import { Box } from '@mui/material';
 import { ReactComponent as MedalIcon_MoyeoRun } from '../../assets/svgs/MedalIcon_MoyeoRun.svg';
+import { ReactComponent as MedalIconSmallBlue_MoyeoRun } from '../../assets/svgs/MedalIconSmallBlue_MoyeoRun.svg';
+import { ReactComponent as MedalIconSmallWhite_MoyeoRun } from '../../assets/svgs/MedalIconSmallWhite_MoyeoRun.svg';
+
+const IndividualRankingBadge = ({ rank }) => (
+  <Box css={indrankingBadge}>
+    <Box css={badgeContainer}>
+      {rank === 1 && <MedalIcon_MoyeoRun css={indIconContainer} />}
+      {/* {rank === 2 && <MedalIcon_MoyeoRun css={indIconContainer} />} */}
+      <Box css={textContainer}>{rank}위</Box>
+    </Box>
+  </Box>
+);
+
+const DevidedMapRankingBadge = ({ rank }) => (
+  <Box css={divrankingBadge(rank)}>
+    <Box css={badgeContainer}>
+      {rank === 1 && <MedalIconSmallWhite_MoyeoRun css={indIconContainer} />}
+      {rank === 2 && <MedalIconSmallBlue_MoyeoRun css={indIconContainer} />}
+      <Box css={textContainer}> {rank}위</Box>
+    </Box>
+  </Box>
+);
 
 const RankingBadge = ({ rank, divMap }) => {
   return (
-    <Box css={rankingBadge(divMap, rank)}>
-      <Box css={badgeContainer}>
-        {rank === 1 && (
-          <Box css={iconContainer(divMap, rank)}>
-            <MedalIcon_MoyeoRun />
-          </Box>
-        )}
-        <Box css={rankText}>{rank}위</Box>
-      </Box>
-    </Box>
+    <>
+      {divMap ? (
+        <DevidedMapRankingBadge rank={rank} divMap={divMap} />
+      ) : (
+        <IndividualRankingBadge rank={rank} />
+      )}
+    </>
   );
 };
 export default RankingBadge;
 
-const rankingBadge = (divMap, rank) => css`
+const indrankingBadge = css`
   height: 33px;
-  width: ${divMap ? 52 : 64}px;
+  width: 64px;
+  border-radius: 3px;
+  box-sizing: border-box;
+  color: #ffffff;
+  background-color: #1162ff;
+  font-family: Apple SD Gothic Neo;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: -0.015em;
+  text-align: center;
+`;
+
+const divrankingBadge = (rank) => css`
+  height: 33px;
+  width: 48px;
   border-radius: 3px;
   box-sizing: border-box;
   color: ${rank > 1 ? '#1162ff' : '#ffffff'};
   background-color: ${rank > 1 ? '#ffffff' : '#1162ff'};
+  font-family: Apple SD Gothic Neo;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  letter-spacing: -0.015em;
+  text-align: left;
+
+  line-height: 20px;
 `;
 
 const badgeContainer = css`
@@ -35,28 +77,12 @@ const badgeContainer = css`
   justify-content: center;
   align-items: center;
   flex-direction: row;
+  padding: 4px;
+  box-sizing: border-box;
 `;
-const iconContainer = (divMap, rank) => css`
-  margin-right: ${!divMap && 5}px;
-  transform: ${divMap && 'scale(0.8)'};
+const indIconContainer = css`
+  margin: auto 4px auto 0px;
 `;
-
-const rankText = (divMap) => css`
-  ${divMap
-    ? `
-    font-family: Apple SD Gothic Neo;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 500;
-    letter-spacing: -0.015em;
-    text-align: left;
-    `
-    : `
-    font-family: Apple SD Gothic Neo;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 500;
-    letter-spacing: -0.015em;
-    text-align: left;
-    `}
+const textContainer = css`
+  margin: auto 0px auto 0px;
 `;
